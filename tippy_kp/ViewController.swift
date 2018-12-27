5af4186d4e38708356c294b21f67016b4cf7c110
TruongKevinPham <truongkevinpham@gmail.com>
2018-12-27 11:38:25 -0800

    Initial Commit

---
+++ tippy_kp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy_kp
+//
+//  Created by Kevin Pham on 12/27/18.
+//  Copyright © 2018 Kevin Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
